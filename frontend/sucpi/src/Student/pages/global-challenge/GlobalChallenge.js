d175aa491a9edffb80a9f13cf47c455005cd0881
Taerogrammer <hyung54321@naver.com>
2024-07-20 11:35:32 +0900

    [design] sucpi #3 - 사이드메뉴 화면 연동

---
+++ frontend/sucpi/src/Student/pages/global-challenge/GlobalChallenge.js
@@ -0,0 +1,10 @@
+
+export function GlobalChallenge()
+{
+    return (
+        <div>
+            <h1>글로벌 챌린지</h1>
+            <h2>서비스 준비 중입니다.</h2>
+        </div>
+    )    
+}
--- frontend/sucpi/src/index.js
+++ frontend/sucpi/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './App.css'
 import { Header } from './Student/components/Header/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Sidebar } from './Student/components/Sidebar/Sidebar';
 import { Home } from './Student/pages/Home';
-
-import './App.css'
+import { GlobalChallenge } from './Student/pages/global-challenge/GlobalChallenge';
+import { ICTVolunteer } from './Student/pages/ict-volunteer/ICTVolunteer';
+import { KoreaChallenge } from './Student/pages/korea-challenge/KoreaChallenge';
 
 function App() {
   return (
@@ -16,6 +18,9 @@ function App() {
         <main className="content">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path='global-challenge' element={<GlobalChallenge />} />
+            <Route path='ict-volunteer' element={<ICTVolunteer /> } />
+            <Route path='korea-challenge' element={<KoreaChallenge /> } />
           </Routes>
         </main>
       </div>
