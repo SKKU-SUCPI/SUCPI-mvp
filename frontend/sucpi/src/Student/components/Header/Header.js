85f853e543f7f39bb0897a7415f2043ac5957d23
Taerogrammer <hyung54321@naver.com>
2024-07-22 11:00:40 +0900

    [refactor] sucpi #3 - 그림들 asset으로 이동

--- frontend/sucpi/src/Student/components/Header/Header.js
+++ frontend/sucpi/src/Student/components/Header/Header.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'; // CSS 파일 임포트
 
+// asset
+import background from '../../../assets/background.png'
+import logo from '../../../assets/sw-sucpi.png'
+
 export function Header() {
 
     return (
@@ -9,7 +13,7 @@ export function Header() {
             <nav className="header bg-body-tertiary">
                 <div className="container">
                     <Link className="header-brand" to="/">
-                        <img src="/sw-sucpi.png" alt="성균관대학교 SW중심대학사업단" width="355" height="86" />
+                        <img src={logo} alt="성균관대학교 SW중심대학사업단" width="355" height="86" />
                     </Link>
                     
                     {/* 로그인 관련 수정 필요. -> 현재는 로그인이 되어있다고 가정하고 개발 중 */}
@@ -20,7 +24,7 @@ export function Header() {
                 </div>
             </nav>
             <div className="background-image">
-                <img src="/background.png" alt="성균관대학교 SW중심대학사업단 SUCPI" />
+                <img src={background} alt="성균관대학교 SW중심대학사업단 SUCPI" />
             </div>
         </header>
     );
