7acfd5b558bbf173c957bba5c6e1b02724acf67c
Taerogrammer <hyung54321@naver.com>
2024-07-22 10:54:54 +0900

    [design] sucpi #3 - 사이드바 토글 버튼 생성

--- frontend/sucpi/src/Student/components/Sidebar/Sidebar.js
+++ frontend/sucpi/src/Student/components/Sidebar/Sidebar.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Sidebar.css'; // CSS 파일 임포트
 
-export function Sidebar()
-{
+export function Sidebar({ toggleSidebar, isOpen }) {
     return (
-        <nav className="sidebar">
-            <ul className="menu">
-                <li><Link to="/global-challenge">글로벌 챌린지</Link></li>
-                <li><Link to="/ict-volunteer">ICT 해외봉사</Link></li>
-                <li><Link to="/korea-challenge">코리아 챌린지</Link></li>
-            </ul>
-        </nav>
+        <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
+            <button className="toggle-button" onClick={toggleSidebar}>
+                {isOpen ? '접기' : '펴기'}
+            </button>
+            <nav>
+                <ul className="menu">
+                    <li><Link to="/global-challenge">글로벌 챌린지</Link></li>
+                    <li><Link to="/ict-volunteer">ICT 해외봉사</Link></li>
+                    <li><Link to="/korea-challenge">코리아 챌린지</Link></li>
+                </ul>
+            </nav>
+        </div>
     );
 }
--- frontend/sucpi/src/index.js
+++ frontend/sucpi/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css'
-import { Header } from './Student/components/Header/Header';
+import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Header } from './Student/components/Header/Header';
 import { Sidebar } from './Student/components/Sidebar/Sidebar';
 import { Home } from './Student/pages/Home';
 import { GlobalChallenge } from './Student/pages/global-challenge/GlobalChallenge';
@@ -11,18 +11,24 @@ import { KoreaChallenge } from './Student/pages/korea-challenge/KoreaChallenge';
 import { Profile } from './Student/pages/profile/Profile';
 
 function App() {
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <BrowserRouter>
       <Header />
       <div className="main-layout">
-        <Sidebar />
+        <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
         <main className="content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path='global-challenge' element={<GlobalChallenge />} />
-            <Route path='ict-volunteer' element={<ICTVolunteer /> } />
-            <Route path='korea-challenge' element={<KoreaChallenge /> } />
-            <Route path='profile' element={<Profile />} />
+            <Route path="global-challenge" element={<GlobalChallenge />} />
+            <Route path="ict-volunteer" element={<ICTVolunteer />} />
+            <Route path="korea-challenge" element={<KoreaChallenge />} />
+            <Route path="profile" element={<Profile />} />
           </Routes>
         </main>
       </div>
