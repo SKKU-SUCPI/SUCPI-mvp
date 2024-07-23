331a397eb757c965b013ebba30eb74afc3b795e9
Taerogrammer <hyung54321@naver.com>
2024-07-23 10:43:09 +0900

    [feature] sucpi #5 - 3Q 컨테이너 생성 및 기타 수정

---
+++ frontend/sucpi/src/Student/pages/profile/LQInfo.js
@@ -0,0 +1,30 @@
+import './AccordionItem'
+
+export function LQInfo()
+{
+    return (
+        <>
+            <div className='form-group'>
+                <label>이름</label>
+                <input type='text' className='form-control' />
+            </div>
+            <div className='form-group'>
+                <label>학번</label>
+                <input type='text' className='form-control' />
+            </div>
+            <div className='form-group'>
+                <label>소속 학과</label>
+                <select className='form-control'>
+                    <option>학과를 선택해 주세요</option>
+                    <option value="소프트웨어학과">소프트웨어학과</option>
+                    <option value="글로벌융합학부">글로벌융합학부</option>
+                    <option value="지능형 소프트웨어학과">지능형 소프트웨어학과</option>
+                </select>
+            </div>
+            <div className='form-group'>
+                <label>연락처</label>
+                <input type='text' className='form-control-contact' /> - <input type='text' className='form-control' /> - <input type='text' className='form-control' />
+            </div>
+        </>
+    );
+}
--- frontend/sucpi/src/Student/pages/profile/Profile.js
+++ frontend/sucpi/src/Student/pages/profile/Profile.js
@@ -1,20 +1,28 @@
 import React from "react";
 import { AccordionItem } from "./AccordionItem";
 import { PersonalInfo } from "./PersonalInfo";
+import { LQInfo } from "./LQInfo";
+import { RQInfo } from "./RQInfo";
+import { CQInfo } from "./CQInfo";
+
 
 
 export function Profile()
 {
     return (
-        <div className="profile" style={{backgroundColor: 'red', marginRight: '30%'}}>
+        <div className="profile" style={{backgroundColor: '#F0F0F0', marginRight: '30%'}}>
             <h1>내 정보</h1>
             <AccordionItem title="개인정보">
                 <PersonalInfo />
             </AccordionItem>
             <AccordionItem title="교과활동">
-                <h1>
-                    개인 정보
-                </h1>
+                <LQInfo />
+            </AccordionItem>
+            <AccordionItem title="교과활동">
+                <RQInfo />
+            </AccordionItem>
+            <AccordionItem title="교과활동">
+                <CQInfo />
             </AccordionItem>
         </div>        
     );
