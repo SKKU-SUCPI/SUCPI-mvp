129f250d47dd037083b0a1c046e0e8655d202aa5
Taerogrammer <hyung54321@naver.com>
2024-07-23 10:42:22 +0900

    [design] sucpi #5 - 형식 추가

--- frontend/sucpi/src/Student/pages/profile/PersonalInfo.js
+++ frontend/sucpi/src/Student/pages/profile/PersonalInfo.js
@@ -4,26 +4,28 @@ export function PersonalInfo()
 {
     return (
         <>
-            <div className='form-group'>
-                <label>이름</label>
-                <input type='text' className='form-control' />
-            </div>
-            <div className='form-group'>
-                <label>학번</label>
-                <input type='text' className='form-control' />
-            </div>
-            <div className='form-group'>
-                <label>소속 학과</label>
-                <select className='form-control'>
-                    <option>학과를 선택해 주세요</option>
-                    <option value="소프트웨어학과">소프트웨어학과</option>
-                    <option value="글로벌융합학부">글로벌융합학부</option>
-                    <option value="지능형 소프트웨어학과">지능형 소프트웨어학과</option>
-                </select>
-            </div>
-            <div className='form-group'>
-                <label>연락처</label>
-                <input type='text' className='form-control-contact' /> - <input type='text' className='form-control' /> - <input type='text' className='form-control' />
+            <div className='form-container'>
+                <div className='form-group'>
+                    <label>이름</label>
+                    <input type='text' className='form-control' />
+                </div>
+                <div className='form-group'>
+                    <label>학번</label>
+                    <input type='text' className='form-control' />
+                </div>
+                <div className='form-group'>
+                    <label>소속 학과</label>
+                    <select className='form-control'>
+                        <option>학과를 선택해 주세요</option>
+                        <option value="소프트웨어학과">소프트웨어학과</option>
+                        <option value="글로벌융합학부">글로벌융합학부</option>
+                        <option value="지능형 소프트웨어학과">지능형 소프트웨어학과</option>
+                    </select>
+                </div>
+                <div className='form-group'>
+                    <label>연락처</label>
+                    <input type='text' className='form-control-contact' /> - <input type='text' className='form-control' /> - <input type='text' className='form-control' />
+                </div>
             </div>
         </>
     );
