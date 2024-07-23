8aed00da0953b2a198fdd42f12da505d15abeb82
Taerogrammer <hyung54321@naver.com>
2024-07-23 09:48:02 +0900

    [design] sucpi #5 - 개인정보 폼 디자인

--- frontend/sucpi/src/Student/pages/profile/Profile.js
+++ frontend/sucpi/src/Student/pages/profile/Profile.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { AccordionItem } from "./AccordionItem";
+import './Profile.css'
+import { PersonalInfo } from "./PersonalInfo";
+
 
 export function Profile()
 {
     return (
-        <div>
+        <div className="profile">
             <h1>내 정보</h1>
-            <h2>서비스 준비 중입니다.</h2>
+            <AccordionItem title="개인정보">
+                <PersonalInfo />
+            </AccordionItem>
+            <AccordionItem title="교과활동">
+                <h1>
+                    개인 정보
+                </h1>
+            </AccordionItem>
         </div>        
     );
 }
