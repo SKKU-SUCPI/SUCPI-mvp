478e7453ab1a744b76191c046a9a04dcdf3cdb28
Taerogrammer <hyung54321@naver.com>
2024-07-24 17:54:45 +0900

    [design] sucpi #9 - 테이블 생성

--- frontend/sucpi/src/Admin/leaderboard/Leaderboard.js
+++ frontend/sucpi/src/Admin/leaderboard/Leaderboard.js
@@ -1,4 +1,5 @@
 import { FilterTable } from "../../components/FilterTable/FilterTable";
+import { Rank } from "./Rank";
 
 
 export function Leaderboard()
@@ -7,6 +8,7 @@ export function Leaderboard()
         <div>
             <h1 style={{padding:"16px 36px 12px"}}>리더보드</h1>
             <FilterTable />
+            <Rank />
         </div>
     );
 }
