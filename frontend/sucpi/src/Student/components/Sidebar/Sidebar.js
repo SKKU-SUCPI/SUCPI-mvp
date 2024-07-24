cb0cb3df3cdd4235c29688d00a7dea2f3ec66ee7
Taerogrammer <hyung54321@naver.com>
2024-07-24 15:18:28 +0900

    [feat] sucpi #7 - 관리자 변경 및 Route 이동

--- frontend/sucpi/src/Student/components/Sidebar/Sidebar.js
+++ frontend/sucpi/src/Student/components/Sidebar/Sidebar.js
@@ -1,9 +1,13 @@
+// Sidebar.jsx
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Sidebar.css'; // CSS 파일 임포트
 import { IoIosArrowDropright, IoIosArrowDropleft } from "react-icons/io";
 
 export function Sidebar({ toggleSidebar, isOpen }) {
+    const location = useLocation();
+    const isAdminPath = location.pathname.startsWith('/admin');
+
     return (
         <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
             <button className="toggle-button" onClick={toggleSidebar}>
@@ -11,9 +15,19 @@ export function Sidebar({ toggleSidebar, isOpen }) {
             </button>
             <nav>
                 <ul className="menu">
-                    <li><Link to="/global-challenge">글로벌 챌린지</Link></li>
-                    <li><Link to="/ict-volunteer">ICT 해외봉사</Link></li>
-                    <li><Link to="/korea-challenge">코리아 챌린지</Link></li>
+                    {isAdminPath ? (
+                        <>
+                            <li><Link to="/admin/leaderboard">리더보드</Link></li>
+                            <li><Link to="/admin/statistic">통계</Link></li>
+                            <li><Link to="/admin/setting">설정</Link></li>
+                        </>
+                    ) : (
+                        <>
+                            <li><Link to="/global-challenge">글로벌 챌린지</Link></li>
+                            <li><Link to="/ict-volunteer">ICT 해외봉사</Link></li>
+                            <li><Link to="/korea-challenge">코리아 챌린지</Link></li>
+                        </>
+                    )}
                 </ul>
             </nav>
         </div>
