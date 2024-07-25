b7f58f3ea275fc3175bd0b674231ceb55add1f71
Taerogrammer <hyung54321@naver.com>
2024-07-25 11:54:52 +0900

    [design] sucpi #9 - 그래프생성

--- frontend/sucpi/src/Admin/statistic/Statistic.js
+++ frontend/sucpi/src/Admin/statistic/Statistic.js
@@ -1,4 +1,7 @@
 import { FilterTable } from "../../components/FilterTable/FilterTable";
+import { DepartmentChart } from "../../components/Graph/PieGraph/DepartmentChart";
+import { GradeChart } from "../../components/Graph/PieGraph/GradeChart";
+import { ThreeQChart } from "../../components/Graph/PieGraph/ThreeQChart";
 
 export function Statistic()
 {
@@ -8,6 +11,11 @@ export function Statistic()
             <h3 style={{padding:"32px 36px 12px"}}>필터</h3>
             <FilterTable />
             <h3 style={{padding:"48px 36px 12px"}}>그래프</h3>
+            <div className="charts-container">
+                <ThreeQChart />
+                <DepartmentChart />
+                <GradeChart />
+            </div>
         </div>
     );
 }
