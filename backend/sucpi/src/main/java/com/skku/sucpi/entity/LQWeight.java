35d1860976b185d574ae6a915d0823c724d523e9
2020311920 <kangsc9335@gmail.com>
2024-07-25 11:14:15 +0900

    [feat] sucpi #12 - LRC Weight테이블 생성

---
+++ backend/sucpi/src/main/java/com/skku/sucpi/entity/LQWeight.java
@@ -0,0 +1,25 @@
+package com.skku.sucpi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "lq_weight")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LQWeight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String dataname;
+    private String name;
+    private float weight;
+}
---
+++ backend/sucpi/src/main/java/com/skku/sucpi/entity/RQWeight.java
@@ -0,0 +1,25 @@
+package com.skku.sucpi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "rq_weight")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RQWeight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String dataname;
+    private String name;
+    private float weight;
+}
