4419aaae2f998a29d522e3e27ada7bee83c2fdbb
2020311920 <kangsc9335@gmail.com>
2024-07-29 09:40:19 +0900

    api response, corconfig 추가

---
+++ backend/sucpi/src/main/java/com/skku/sucpi/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.skku.sucpi;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private int status;
+    private String message;
+    private T result;
+}
