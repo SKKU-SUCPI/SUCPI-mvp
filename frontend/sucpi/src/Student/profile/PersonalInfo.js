6d261232c5a135bfa71db2e7899face57098369c
Taerogrammer <hyung54321@naver.com>
2024-07-29 15:12:25 +0900

    [feat] sucpi #25 - 개인정보 연락처 변경 기능 추가

--- frontend/sucpi/src/Student/profile/PersonalInfo.js
+++ frontend/sucpi/src/Student/profile/PersonalInfo.js
@@ -1,29 +1,68 @@
-import './AccordionItem'
+import React from 'react';
+import './AccordionItem.css';
 
-export function PersonalInfo()
-{
+export function PersonalInfo({ studentInfo, editable }) {
+    const inputStyle = editable ? { backgroundColor: 'white' } : {};
+    
+    // 매핑 테이블을 생성하여 studentMajor 값을 소속 학과 이름으로 변환
+    const majorMapping = {
+        "SW": "소프트웨어학과",
+        "GC": "글로벌융합학부",
+        "AI": "지능형 소프트웨어학과"
+    };
+
     return (
         <div className='form-container'>
             <div className='form-group form-group-row'>
                 <label>이름</label>
-                <input type='text' className='form-control' />
+                <input 
+                    type='text' 
+                    className='form-control' 
+                    defaultValue={studentInfo.studentName} 
+                    disabled={true} 
+                />
             </div>
             <div className='form-group form-group-row'>
                 <label>학번</label>
-                <input type='text' className='form-control' />
+                <input 
+                    type='text' 
+                    className='form-control' 
+                    defaultValue={studentInfo.studentId} 
+                    disabled={true} 
+                />
             </div>
             <div className='form-group form-group-row'>
                 <label>소속 학과</label>
-                <select className='form-control'>
-                    <option>학과를 선택해 주세요</option>
-                    <option value="소프트웨어학과">소프트웨어학과</option>
-                    <option value="글로벌융합학부">글로벌융합학부</option>
-                    <option value="지능형 소프트웨어학과">지능형 소프트웨어학과</option>
-                </select>
+                <input 
+                    type='text' 
+                    className='form-control' 
+                    defaultValue={majorMapping[studentInfo.studentMajor] || "학과를 선택해 주세요"} 
+                    disabled={true} 
+                />
             </div>
             <div className='form-group form-group-row'>
-                <label>연락처</label>
-                <input type='text' className='form-control-contact' /> - <input type='text' className='form-control' /> - <input type='text' className='form-control' />
+                <label style={{ whiteSpace: "nowrap" }}>연락처</label>
+                <input 
+                    type='text' 
+                    className='form-control-contact' 
+                    defaultValue={studentInfo.studentPhoneNum.split('-')[0]} 
+                    disabled={!editable} 
+                    style={inputStyle} 
+                /> - 
+                <input 
+                    type='text' 
+                    className='form-control' 
+                    defaultValue={studentInfo.studentPhoneNum.split('-')[1]} 
+                    disabled={!editable} 
+                    style={inputStyle} 
+                /> - 
+                <input 
+                    type='text' 
+                    className='form-control' 
+                    defaultValue={studentInfo.studentPhoneNum.split('-')[2]} 
+                    disabled={!editable} 
+                    style={inputStyle} 
+                />
             </div>
         </div>
     );
--- frontend/sucpi/src/Student/profile/Profile.js
+++ frontend/sucpi/src/Student/profile/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { AccordionItem } from "./AccordionItem";
 import { PersonalInfo } from "./PersonalInfo";
 import { LQInfo } from "./LQInfo";
@@ -8,29 +8,36 @@ import './Profile.css'
 import menuImage from '../../assets/menu.png'
 
 
-
-export function RightMenu() {
+export function RightMenu({ editable, onSaveClick, onEditClick }) {
     return (
         <div className="right-menu">
             <div className="right-menu-header">
                 <img src={menuImage} alt="SUCPI Logo" className="circle-logo-image" />
             </div>
             <div className="right-menu-body">
-                <button className="right-menu-button add-button">추가하기</button>
-                <button className="right-menu-button edit-button">수정하기</button>
+                <button className="right-menu-button add-button" onClick={onSaveClick}>저장하기</button>
+                <button className="right-menu-button edit-button" onClick={onEditClick}>수정하기</button>
             </div>
         </div>
     );
 }
 
-export function Profile()
-{
+export function Profile() {
+    const [editable, setEditable] = useState(false);
+    const [studentInfoData, setStudentInfoData] = useState(data.result.studentInfo);
+
+    const handleEditClick = () => setEditable(true);
+    const handleSaveClick = () => {
+        setEditable(false);
+        alert("저장이 완료되었습니다.");
+    };
+
     return (
         <div className="profile-container">
             <div className="profile" style={{ backgroundColor: '#F0F0F0', marginRight: '30%' }}>
                 <h1>내 정보</h1>
                 <AccordionItem title="개인정보">
-                    <PersonalInfo />
+                    <PersonalInfo studentInfo={studentInfoData} editable={editable} />
                 </AccordionItem>
                 <AccordionItem title="교과활동">
                     <LQInfo />
@@ -42,7 +49,79 @@ export function Profile()
                     <CQInfo />
                 </AccordionItem>
             </div>
-            <RightMenu />
+            <RightMenu onSaveClick={handleSaveClick} onEditClick={handleEditClick} editable={editable} />
         </div>
     );
 }
+
+
+
+// 더미 데이터
+const data = {
+    "status": 200,
+    "message": "Student details retrieved successfully",
+    "result": {
+        "CQInfo": {
+            "CQCoop": 0,
+            "CQInternship": 0,
+            "CQStartup": 0,
+            "CQStudentCouncil": 0,
+            "CQLectureSeminar": 0,
+            "CQOverseaVolunteer": 0,
+            "CQMediaPromotion": 0,
+            "CQStudyGroup": 0,
+            "CQStudioContribution": 0,
+            "CQAlimi": 0
+        },
+        "studentInfo": {
+            "studentId": "2020123123",
+            "studentMajor": "SW",
+            "studentName": "홍길동",
+            "studentPhoneNum": "010-1234-5678"
+        },
+        "RQInfo": {
+            "RQYulNationalPoster": 0,
+            "RQYulPlaySchoolCompetition": 0,
+            "RQYulWinBigCompetition": 0,
+            "RQYulJcr10Main": 0,
+            "RQMyeongNationalSpeech": 0,
+            "RQMyeongBigCompetition": 0,
+            "RQYulJcr5Main": 0,
+            "RQYulKnownSpeech": 0,
+            "RQYulPlayBigCompetition": 0,
+            "RQYulNormalSpeech": 0,
+            "RQYulJcr5Part": 0,
+            "RQMyeongNormalSpeech": 0,
+            "RQYulJcr20Part": 0,
+            "RQMyeongOverKci": 0,
+            "RQYulNationalSpeech": 0,
+            "RQYulJcr20Main": 0,
+            "RQMyeongKnownSpeech": 0,
+            "RQYulKnownPoster": 0,
+            "RQYulNormalPoster": 0,
+            "RQMyeongKciExcellent": 0,
+            "RQMyeongSchoolCompetition": 0,
+            "RQYulJcr10Part": 0,
+            "RQYulWinSchoolCompetition": 0,
+            "RQMyeongKci": 0,
+            "RQMyeongKciCandidate": 0
+        },
+        "LQInfo": {
+            "LQGrade35TO40": 0,
+            "LQOpenSourceActivity1Star4": 0,
+            "LQOpenSourceActivity2Star2": 0,
+            "LQGrade30TO35": 0,
+            "LQOpenSourceActivity1Star5": 0,
+            "LQOpenSourceActivity2Star1": 0,
+            "LQOpenSourceActivity1Star2": 0,
+            "LQOpenSourceActivity1Star3": 0,
+            "LQEduActivity1": 5,
+            "LQOpenSourceActivity1Star1": 0,
+            "LQOpenSourceActivity2Star5": 0,
+            "LQOpenSourceActivity2Star4": 0,
+            "LQEduActivity2": 0,
+            "LQOpenSourceActivity2Star3": 0,
+            "LQGrade40TO45": 0
+        }
+    }
+};
