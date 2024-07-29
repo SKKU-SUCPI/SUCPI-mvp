c16fe3d38c22af4618d28b0f578004a8756b397b
somehowranker <96857279+somehowranker@users.noreply.github.com>
2024-07-29 17:15:57 +0900

    [feat](SUCPI)#20 - LQ,RQ,CQ 비율 저장 및 수정 기능 구현

--- backend/sucpi/src/main/java/com/skku/sucpi/repository/LRCRatioRepository.java
+++ backend/sucpi/src/main/java/com/skku/sucpi/repository/LRCRatioRepository.java
@@ -1,5 +1,7 @@
 package com.skku.sucpi.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.skku.sucpi.entity.LRCRatio;
 
 @Repository
 public interface LRCRatioRepository extends JpaRepository<LRCRatio, Long> {
+    Optional<LRCRatio> findTopByOrderByIdAsc();
 }
-
