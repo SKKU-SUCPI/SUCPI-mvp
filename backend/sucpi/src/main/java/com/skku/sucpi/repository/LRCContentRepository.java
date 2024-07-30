456a02e2d7059cb410b4fc5dbbfc6c11629c6ce2
2020311920 <kangsc9335@gmail.com>
2024-07-30 12:33:53 +0900

    [feat] #23 - 학생 api 및 건 수, 점수 계산 완료

--- backend/sucpi/src/main/java/com/skku/sucpi/repository/LRCContentRepository.java
+++ backend/sucpi/src/main/java/com/skku/sucpi/repository/LRCContentRepository.java
@@ -1,5 +1,7 @@
 package com.skku.sucpi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,7 @@ import com.skku.sucpi.entity.LRCContent;
 
 @Repository
 public interface LRCContentRepository extends JpaRepository<LRCContent, Long> {
+    List<LRCContent> findByStudentId(String studentId);
+
+    void deleteByStudentId(String studentId);
 }
