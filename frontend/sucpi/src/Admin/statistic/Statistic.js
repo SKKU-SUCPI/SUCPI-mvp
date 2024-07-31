e5bd968c36ea6f401fe404def2bfc66f20473aa3
Taerogrammer <hyung54321@naver.com>
2024-07-31 17:38:04 +0900

    [feat] sucpi #50 - 통계부분 주석처리

--- frontend/sucpi/src/Admin/statistic/Statistic.js
+++ frontend/sucpi/src/Admin/statistic/Statistic.js
@@ -11,7 +11,8 @@ export function Statistic()
         <div>
             <h1 style={{padding:"16px 36px 12px"}}>통계</h1>
             <h3 style={{padding:"32px 36px 12px"}}>필터</h3>
-            <FilterTable />
+            {/* 추후 진행 */}
+            {/* <FilterTable /> */}
             <h3 style={{padding:"48px 36px 12px"}}>그래프</h3>
             <div className="charts-container">
                 <ThreeQChart />
