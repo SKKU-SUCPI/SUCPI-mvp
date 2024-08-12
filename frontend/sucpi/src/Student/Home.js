e7ac4987524b37fff5d7b652ef3ffa671a7bdcdd
Taerogrammer <hyung54321@naver.com>
2024-08-12 15:30:50 +0900

    [feat] sucpi #78 - 홈 화면 문구 추가

--- frontend/sucpi/src/Student/Home.js
+++ frontend/sucpi/src/Student/Home.js
@@ -1,9 +1,17 @@
-
-export function Home()
-{
+export function Home() {
     return (
+        <>
         <div className="container my-4">
-            <h1>SUCPI 소개글 및 학장님의 훈화 말씀</h1>
+            <h1 style={{ whiteSpace: "nowrap", marginBottom: "20px" }}>SUCPI 소개</h1>
         </div>
+        <div>
+            <p>
+                SUCPI는 성균관대학교 SW중심대학사업단에서 개발한 학생 역량지수로서, 학생의 교육, 연규 및 교류 활동(봉사, 산학협력 등)을 지표화하여 측정한 값입니다.
+            </p>
+            <p>
+                학생들의 역량을 객관적으로 평가하여, 진학지도, 비교과 과정 선발 우선권 등의 보상성 지원과 학술 동기를 촉진하는 데 목적을 두고 있습니다.
+            </p>
+        </div>
+        </>
     );
 }
