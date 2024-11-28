828eac6c1293edafc6132d7061e421105932a377
Taerogrammer <hyung54321@naver.com>
2024-11-28 14:24:02 +0900

    8080 -> 4502로 변경

--- backend/sucpi/src/main/java/com/skku/sucpi/CorsConfig.java
+++ backend/sucpi/src/main/java/com/skku/sucpi/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:4501", "http://siop-dev.skku.edu:8080")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:4501", "http://siop-dev.skku.edu:4502")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
