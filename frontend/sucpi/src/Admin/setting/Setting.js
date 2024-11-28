3fb08eb2f9d21d649619cbac6b9580d90f431cd2
Taerogrammer <hyung54321@naver.com>
2024-11-28 14:18:54 +0900

    setting url 변경

--- frontend/sucpi/src/Admin/setting/Setting.js
+++ frontend/sucpi/src/Admin/setting/Setting.js
@@ -9,7 +9,7 @@ export function Setting() {
     const [detailData, setDetailData] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:8080/api/admin/settings')
+        fetch('http://siop-dev.skku.edu:4502/api/admin/settings')
             .then(response => response.json())
             .then(data => {
                 if (data.status === 200) {
