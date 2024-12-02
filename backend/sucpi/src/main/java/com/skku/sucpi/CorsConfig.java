5f1fc55823f2ddcb450adf7789fe3927971d65b8
Taerogrammer <hyung54321@naver.com>
2024-12-02 09:36:42 +0900

    CorsConfig url 추가

--- backend/sucpi/src/main/java/com/skku/sucpi/CorsConfig.java
+++ backend/sucpi/src/main/java/com/skku/sucpi/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4501", "http://siop-dev.skku.edu:8080")
+                        .allowedOrigins("http://localhost", "http://localhost:4501", "http://siop-dev.skku.edu:8080")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
